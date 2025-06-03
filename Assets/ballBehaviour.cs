f1d07120f88ec38711756e18a61de9867b004387
Selena <benigniselena@gmail.com>
2025-06-03 20:45:34 -0300

    Add restart function

--- Assets/ballBehaviour.cs
+++ Assets/ballBehaviour.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BallBehaviour : MonoBehaviour
 {
     private Rigidbody myRigidbody;
     private Vector3 moveObject;
+    private Transform myTransform;
+    private Vector3 initialPosition;
 
     [SerializeField] private float speed = 10f;
 
     private void Start()
     {
         myRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        myTransform = this.gameObject.GetComponent<Transform>();
+        initialPosition = myTransform.position;
     }
 
     void Update()
@@ -21,10 +26,13 @@ public class BallBehaviour : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.useGravity = true;
+            myRigidbody.constraints = RigidbodyConstraints.FreezePositionX;
         }
 
         if (!myRigidbody.useGravity)
         {
+            myRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX;
+
             if (Input.GetKey(KeyCode.A))
             {
                 moveObject.z += 1;
@@ -39,5 +47,11 @@ public class BallBehaviour : MonoBehaviour
         Vector3 objectPosition = transform.position;
         objectPosition += moveObject.normalized * speed * Time.deltaTime;
         transform.position = objectPosition;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myRigidbody.useGravity = false;
+            transform.position = initialPosition;
+        }
     }
 }
