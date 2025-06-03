f8105f1b65767cc245883274efa16adc27011713
Selena <benigniselena@gmail.com>
2025-06-03 18:19:00 -0300

    Add ball movement

--- Assets/ballBehaviour.cs
+++ Assets/ballBehaviour.cs
@@ -5,20 +5,40 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
     private Rigidbody myRigidbody;
-    private Transform myTransform;
+    private Vector3 moveObject;
+
+    [SerializeField] private float speed = 10f;
 
     private void Start()
     {
         myRigidbody = this.gameObject.GetComponent<Rigidbody>();
-        myTransform = this.gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveObject = Vector3.zero;
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.useGravity = true;
         }
+
+        if (!myRigidbody.useGravity)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveObject.z += 1;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveObject.z -= 1;
+            }
+        }
+
+        Vector3 myPos = transform.position;
+        myPos += moveObject.normalized * speed * Time.deltaTime;
+        transform.position = myPos;
     }
 }
